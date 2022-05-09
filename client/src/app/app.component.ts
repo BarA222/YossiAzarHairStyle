2bd56864dfd2b0a7e48cfaf85822b38b85ddf940
BarAz <barazriel2@gmail.com>
2022-05-09 12:14:40 +0300

    setting bootstrap

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'client';
+  title = 'Yossi Azar';
+
+  constructor (private http: HttpClient){
+  
+  }
+
+  ngOnInit(): void{
+
+  }
 }
+
+
